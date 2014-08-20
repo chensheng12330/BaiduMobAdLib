6465694cbe230b6685fd4cd925db9ef65911ee09
chensheng12330 <chensheng12330@gmail.com>
2014-08-20 16:55:27 +0800

    all

---
+++ APIExampleApp/BaiduMobAdAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  BaiduMobAdAppDelegate.h
+//  APIExampleApp
+//
+//  Created by jaygao on 11-10-26.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface BaiduMobAdAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UIViewController *viewController;
+
+@end
---
+++ APIExampleApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  APIExampleApp
+//
+//  Created by jaygao on 11-10-26.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BaiduMobAdAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BaiduMobAdAppDelegate class]));
+    }
+}
